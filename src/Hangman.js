0f37ede2e16a4a417a780b1c4b62347f001a770f
Julio Lopez <joeyclopez88#gmail.com>
2021-04-30 10:51:54 -0700

    added the guessed wrong section and the you lose functionality

--- src/Hangman.js
+++ src/Hangman.js
@@ -46,6 +46,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -61,8 +62,12 @@ class Hangman extends Component {
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
+        <p>Guesssed Wrong: {this.state.nWrong} </p>
         <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className='Hangman-btns'>
+          {this.state.nWrong < this.props.maxWrong ? this.generateButtons() : `You Lose: ${this.state.answer}`}
+          {this.generateButtons()}
+        </p>
       </div>
     );
   }
